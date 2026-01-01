60a2626579d83feb5d66f359fe416282aa9cdc17
Methre Amarnath <methreamarnath@gmail.com>
2026-01-01 20:45:42 +0530

    Add TypeScript config and CLI executable
    - Configure TypeScript compiler options for ESM
    - Add bin/awesomediagns.js executable wrapper
    - Set up build pipeline for CLI tool

---
+++ bin/awesomediagns.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+/**
+ * codereviewer.ai (AwesomeDiagns)
+ * This is the executable wrapper for the CLI.
+ * It points to the compiled entry point in the dist folder.
+ */
+
+import "../dist/index.js";
